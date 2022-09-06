eef9034bcbc5d34a047b4d5a61f05d7adea156a4
rezatangse <rezatangse@gmail.com>
2022-09-06 21:54:18 +0700

    1 file modified, 1 added

--- src/Page.js
+++ src/Page.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Header } from './Header';
+import Counter from './Counter';
 
 export default function Page() {
   const name = 'Reza';
   return (
     <>
       <Header name={name} />
-      <p>Hai</p>
+      <p>Paragraf</p>
+      <Counter />
     </>
   );
 }
