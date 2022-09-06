0532e5dcb51bd3d4a90e6c7bec25fd3de845e02b
rezatangse <rezatangse@gmail.com>
2022-09-06 13:39:48 +0700

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Header() {
+function Header(props) {
   const clock = Date().toLocaleString();
-  return <h1>Hello {clock}</h1>;
+  return <h1>Hello {props.name} {clock}</h1>;
 }
 
 function Page() {
+  const name = 'Reza'
   return (
     <>
-      <Header />
+      <Header name={name} />
       <p>Hai</p>
     </>
   );
